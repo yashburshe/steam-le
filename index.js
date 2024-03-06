d62e7677d4ff514e115fe4e2c4e502aa02c6952c
Yash Burshe <bursheyash2@gmail.com>
2024-03-07 03:35:49 +0530

    Update UI and add account sign in through steam

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
 require("dotenv").config();
-
 var express = require("express");
 var app = express();
-
 var path = require("path");
 var bodyParser = require("body-parser");
 
-let csv = [];
-let nickname;
+//Passport
+var passport = require("passport");
+var util = require("util");
+var session = require("express-session");
+var SteamStrategy = require("passport-steam").Strategy;
 
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+passport.use(
+  new SteamStrategy(
+    {
+      returnURL: "http://localhost:8080/auth/steam/return",
+      realm: "http://localhost:8080/",
+      apiKey: process.env.KEY,
+    },
+    function (identifier, profile, done) {
+      process.nextTick(function () {
+        profile.identifier = identifier;
+        return done(null, profile);
+      });
+    }
+  )
+);
+
+//Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    name: "steam_auth",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, //7 day expiry for cookies
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set("view engine", "ejs");
 app.set("port", process.env.PORT);
 
-app.get("/", function (req, res) {
-  res.render("index");
-});
+let csv = [];
+let nickname;
 
-app.get("/help", function (req, res) {
-  res.render("help");
-});
-
+//Get Functions
 async function getUser(key, username) {
   let vanityURL =
     "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=" +
@@ -66,6 +99,65 @@ async function getProfile(key, steamid) {
   }
 }
 
+//Auth Related Endpoints
+app.get(
+  "/auth/steam",
+  passport.authenticate("steam", { failureRedirect: "/" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+app.get(
+  "/auth/steam/return",
+  passport.authenticate("steam", { failureRedirect: "/" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+app.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+
+//Endpoints
+app.get("/", function (req, res) {
+  res.render("index", { user: req.user });
+});
+
+app.get("/help", function (req, res) {
+  res.render("help");
+});
+
+app.get("/account", async function (req, res) {
+  if(!req.user){
+    res.redirect('/')
+  }
+  if(req.user){
+    let gamesList = await getGames(process.env.KEY, req.user.id);
+    res.render("account", { user: req.user, gamesList: gamesList });
+  }
+});
+
+app.get("/download", (req, res) => {
+  res
+    .status(200)
+    .attachment(nickname + "_steam.csv")
+    .send(csv);
+});
+
 app.get("/games", async (req, res) => {
   const key = process.env.KEY;
   let steamid;
@@ -73,14 +165,22 @@ app.get("/games", async (req, res) => {
   var username = req.query.id;
   nickname = username;
   if (username == "") {
-    res.render("error", { code: 0, message: "Please enter a username" });
+    res.render("error", {
+      code: 0,
+      message: "Please enter a username",
+      user: req.user,
+    });
   } else {
     if (/^([0-9]{17})$/.test(username)) {
       steamid = username;
     } else {
       let user = await getUser(key, username);
       if (user.response.message == "No match") {
-        res.render("error", { code: 1, message: "No match found" });
+        res.render("error", {
+          code: 1,
+          message: "No match found",
+          user: req.user,
+        });
         return;
       }
       steamid = user.response.steamid;
@@ -90,7 +190,11 @@ app.get("/games", async (req, res) => {
       profile.response.players[0].communityvisibilitystate == 1 ||
       profile.response.players[0].communityvisibilitystate == 2
     ) {
-      res.render("error", { code: 2, message: "Profile is private" });
+      res.render("error", {
+        code: 2,
+        message: "Profile is private",
+        user: req.user,
+      });
     } else {
       let gamesOwned = await getGames(key, steamid);
 
@@ -113,16 +217,10 @@ app.get("/games", async (req, res) => {
         nickname: profile.response.players[0].personaname,
         gamesList: gamesOwned.response.games,
         steamid: steamid,
+        user: req.user,
       });
     }
   }
-});
-
-app.get("/download", (req, res) => {
-  res
-    .status(200)
-    .attachment(nickname + "_steam.csv")
-    .send(csv);
 });
 
 app.listen(process.env.PORT, () => {
